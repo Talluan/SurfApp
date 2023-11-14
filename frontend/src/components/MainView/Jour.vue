3ae675d0629ec8fc3675f9fe6535481629152714
Talluan <tmclaudel@gmail.com>
2023-11-14 15:01:44 +0100

    [frontend] Ajout composants de base

---
+++ frontend/src/components/MainView/Jour.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Je suis un jour</div>
+    <div>sens du vent : {{ sensVent }}</div>
+</template>
+
+<script setup lang="ts">
+
+    import { ref } from 'vue';
+
+
+    const sensVent = ref("Sud");
+
+
+
+</script>
+
---
+++ frontend/src/components/MainView/Prevision.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <Jour/>
+    <Jour/>
+    <Jour/>
+
+</template>
+
+<script setup lang="ts">
+import Jour from './Jour.vue';
+</script>
